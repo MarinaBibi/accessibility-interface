bb70eaa471a3cb0fa0eb08c676fe3066c7db28ab
MarinaBibi <Marinabibi1105@gmail.com>
2026-04-30 11:29:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function readAloud() {
   let text = "Personal details entered.";
-
   let speech = new SpeechSynthesisUtterance(text);
   speechSynthesis.speak(speech);
+}
+
+function toggleTheme() {
+  document.body.classList.toggle("dark-mode");
 }
